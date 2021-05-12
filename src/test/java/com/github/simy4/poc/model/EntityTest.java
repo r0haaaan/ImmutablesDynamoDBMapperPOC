9ebf51ee0072a0d640f5da687263dab12a3b6407
SimY4 <alex.simkin@outlook.com>
2021-05-12 14:44:45 +1000

    Check equality override.

---
+++ src/test/java/com/github/simy4/poc/model/EntityTest.java
@@ -0,0 +1,29 @@
+package com.github.simy4.poc.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EntityTest {
+  private final Entity entity = ImmutableEntity.builder()
+      .tenant("tenant")
+      .name("name")
+      .address(ImmutableAddress.builder()
+          .line1("123 ABC st.")
+          .country("US")
+          .build())
+      .status(Status.ACTIVE)
+      .version(1L)
+      .build();
+  private final Entity newerEntity = ImmutableEntity.copyOf(entity).withVersion(2L).withName("another-name");
+
+  @Test
+  void immutableEntitiesShouldBeEqual() {
+    assertEquals(entity, newerEntity);
+  }
+
+  @Test
+  void modifiableEntitiesShouldBeEqual() {
+    assertEquals(new ModifiableEntity().from(entity), new ModifiableEntity().from(newerEntity));
+  }
+}
